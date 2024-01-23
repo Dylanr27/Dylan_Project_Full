f1e09c9ca2ff68947bfa694709be02d53a407bc1
Dylanr27 <djregister_50@msn.com>
2024-01-22 18:45:39 -0800

    Add project files.

---
+++ Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetAdoptionMVC.Models
+{
+    public class Pet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int age { get; set; }
+        [Required]
+        public string species { get; set; }
+        public string breed { get; set; }
+        public string gender { get; set; }
+        public string size { get; set; }
+        public string description { get; set; }
+        public string AdoptionStatus { get; set; }
+    }
+}
